4a0e037380456a5c09b7a53f001384cdc49dc915
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-03-03 19:38:32 +0100

    Add member field to RegistrationDocument model
    Avoid having documentd without registration nor member.

--- nsigma/members/models/registration_document.py
+++ nsigma/members/models/registration_document.py
@@ -12,5 +12,6 @@ def upload_path(instance, filename):
 class RegistrationDocument(models.Model):
     MAX_NAME_LEN = 50
 
+    member = models.ForeignKey('Member', on_delete=models.CASCADE,)
     name = models.CharField(max_length=MAX_NAME_LEN,)
     file = models.FileField(upload_to=upload_path)
